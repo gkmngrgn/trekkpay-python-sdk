2d9f76204f32bdafbf84550fceafd9b6fcfda23d
Gokmen Gorgen <gokmen@alageek.com>
2018-04-29 21:03:19 +0200

    initial commit.

---
+++ tests/test_transaction.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class TestTransaction(unittest.TestCase):
+    def test_transaction(self):
+        # TODO: it's not a correct test.
+        self.assertEqual('aaa'.upper(), 'AAA')
